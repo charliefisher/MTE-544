d8bbec2e686120eb7de900d715e9471e89cc29bc
Charlie Fisher <charliefisherhome@gmail.com>
2022-11-17 16:05:21 -0500

    First implementation of lidar point ransformation

--- Lab 2/occupancy_map.py
+++ Lab 2/occupancy_map.py
@@ -2,6 +2,7 @@ import math
 import warnings
 
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 class BinaryOccupancyMap:
@@ -16,7 +17,7 @@ class BinaryOccupancyMap:
         return self._map
 
     """Converts world coordinates to indices in the map"""
-    def world_to_map(self, x: float, y: float) -> tuple[float, float]:
+    def world_to_map(self, x: float, y: float) -> tuple[int, int]:
         # map origin is defined at bottom left corner, but indexing starts at the top left corner
         # thus, the y coordinate has to be offset
         max_y_idx: int = self._map.shape[0] - 1
@@ -24,10 +25,39 @@ class BinaryOccupancyMap:
         y_idx = max_y_idx - math.floor((y - self._origin[1])/self._cell_size)
         return (y_idx, x_idx)
 
+    def map_to_world(self, x_idx: int, y_idx: int) -> tuple[float, float]:
+        # map origin is defined at bottom left corner, but indexing starts at the top left corner
+        # thus, the y coordinate has to be offset
+        max_y_idx: int = self._map.shape[0] - 1
+        x = self._origin[0] + self._cell_size*x_idx + self._cell_size/2
+        y = self._origin[1] + self._cell_size*(max_y_idx - y_idx) + self._cell_size/2
+        return (x, y)
+
     """Checks if a given coordinate in world space is occupied"""
     def is_occupied(self, x: float, y: float) -> bool:
         y_idx, x_idx = self.world_to_map(x, y)
         if x_idx < 0 or y_idx < 0 or x_idx >= self._map.shape[1] or y_idx >= self._map.shape[0]:
             warnings.warn("({}, {}) is outside of map!".format(x, y))
-            return True
+            return False
         return bool(self._map[y_idx, x_idx])  # convert np.bool to python bool
+
+    def _plot_map(self) -> None:
+        map_size = self._map.shape
+        extent = (
+            self._origin[0], self._origin[0] + map_size[1]*self._cell_size,
+            self._origin[1], self._origin[1] + map_size[0]*self._cell_size
+        )
+        plt.imshow(np.logical_not(self._map), cmap='gray', extent=extent)
+        plt.xlim((extent[0], extent[1]))
+        plt.ylim((extent[2], extent[3]))
+
+    def plot_points(self, points: np.array) -> None:
+        self._plot_map()
+        plt.scatter(points[:,0], points[:,1], c='r', s=0.2)
+
+    def plot_particles(self, particles: np.array) -> None:
+        self._plot_map()
+        for x, y, theta in particles:
+            plt.scatter(x, y, c='b', s=10)
+            arrow_len = 0.25
+            plt.arrow(x, y, arrow_len*np.cos(theta), arrow_len*np.sin(theta), color='b')
--- Lab 2/particle_filter.py
+++ Lab 2/particle_filter.py
@@ -1,3 +1,8 @@
+import json
+
+import numpy as np
+import matplotlib.pyplot as plt
+
 from occupancy_map import BinaryOccupancyMap
 
 
@@ -5,12 +10,112 @@ MAP_ORIGIN: tuple[float, float] = (-1.94, -8.63)
 MAP_CELL_SIZE: float = 0.03
 
 
-class ParticleFilter:
-    def __init__(self) -> None:
-        self._map = BinaryOccupancyMap('binary_occupancy_map.csv', MAP_ORIGIN, MAP_CELL_SIZE)
+Pose = np.array # 3 x 1 vector
+Poses = np.array # n x 3 matrix
 
-    def run(self) -> tuple[float, float]:
+
+class ParticleFilter:
+    def __init__(self, n_particles: float) -> None:
+        self._n_particles = n_particles
+        self._map = BinaryOccupancyMap('binary_occupancy_map.csv', MAP_ORIGIN, MAP_CELL_SIZE)
+        self._angle_min: float = None
+        self._angle_max: float = None
+        self._angle_increment: float = None
+        self._range_min: float = None
+        self._range_max: float = None
+
+    def _lidar_scan_to_cartesian(self, scan_data: np.array) -> np.array:
+        scan_data = np.clip(scan_data, self._range_min, self._range_max)
+        angle = np.arange(self._angle_min, self._angle_max + self._angle_increment, self._angle_increment)
+        cartesian = (np.array([np.cos(angle), np.sin(angle)])*scan_data).T
+        cartesian_len = cartesian.shape[0]
+        return np.append(cartesian, np.ones((cartesian_len, 1)), axis=1)
+
+    def _generate_inital_particles(self) -> Poses:
+        particles = []
+        while len(particles) < self._n_particles:
+            x = np.random.uniform(-1.94, 8.74)
+            y = np.random.uniform(-8.63, 5.08)
+
+            if self._map.is_occupied(x, y):
+                continue
+
+            theta = np.random.uniform(-np.pi, np.pi)
+            particles.append(np.array([x, y, theta]))
+
+        return np.array(particles)
+
+    def _lidar_frame_to_robot(self, scan_data_cartesian: np.array) -> np.array:
+        rot_ang = -np.pi/2
+        T_robot_lidar = np.array([
+            [np.cos(rot_ang), -np.sin(rot_ang), -0.04],
+            [np.sin(rot_ang), np.cos(rot_ang), 0],
+            [0, 0, 1]
+        ], np.float64)
+        return scan_data_cartesian@T_robot_lidar
+
+    def _robot_frame_to_map(self, robot_pose: Pose, scan_data_robot: np.array) -> np.array:
+        rot_ang = robot_pose[2]
+        T_map_robot = np.array([
+            [np.cos(rot_ang), -np.sin(rot_ang), robot_pose[0]],
+            [np.sin(rot_ang), np.cos(rot_ang), robot_pose[1]],
+            [0, 0, 1]
+        ], np.float64)
+        return scan_data_robot@T_map_robot
+
+    def _weight_particle(self) -> float:
         pass
+
+    def _run(self, scan_data: np.array) -> Pose:
+        scan_data_cartesian = self._lidar_scan_to_cartesian(scan_data)
+
+        scan_data_robot = self._lidar_frame_to_robot(scan_data_cartesian)
+        assert np.all(np.not_equal(scan_data_cartesian, scan_data_robot))  # check transformation succeeded
+        
+        robot_poses = self._generate_inital_particles()
+        print('robot_poses', robot_poses.shape)
+
+        all_points = np.ndarray((len(scan_data)*self._n_particles, 2))
+        for i, pose in enumerate(robot_poses):
+            scan_data_map = self._robot_frame_to_map(pose, scan_data_robot)
+            print('scan_data_map', scan_data_map.shape)
+            assert np.all(np.not_equal(scan_data_robot, scan_data_map))  # check transformation succeeded
+            all_points[i*len(scan_data):(i+1)*len(scan_data)] = scan_data_map[:,0:2]
+            
+            print('robot pose is', pose)
+            plt.figure()
+            self._map.plot_particles([pose])
+            # print('extracted pts', scan_data_map[:,0:2].shape)
+            self._map.plot_points(scan_data_map[:,0:2])
+            plt.show()
+
+        print('all_points', all_points.shape)
+        self._map.plot_points(all_points)
+
+
+
+    def localize(self, lidar_data_file: str) -> tuple[float, float]:
+        # read json file
+        with open(lidar_data_file, 'r') as data_file:
+            data = json.load(data_file)
+
+        # set parameters from sensor
+        self._angle_min = data['angle_min']
+        self._angle_max = data['angle_max']
+        self._angle_increment = data['angle_increment']
+        self._range_min = data['range_min']
+        self._range_max = data['range_max']
+
+        scan_data = data['scan_data']
+
+        # check each lidar scan has the same number of measurements
+        assert all(len(scan_data[0])== len(s) for s in scan_data)
+
+        # run filter where a single iteration is a single record of scan data
+        for scan in scan_data:
+            lidar_measurement = np.array(scan)
+            self._run(lidar_measurement)
+            break # TODO: remove me, just for testing
 
 
 def test():
@@ -33,6 +138,6 @@ def test():
 
 
 if __name__ == '__main__':
-    pf = ParticleFilter()
-    pf.run()
+    pf = ParticleFilter(50)
+    pf.localize('point2.json')
     
